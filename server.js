9d59be0f930412f46320f1c109e9df921deea2dc
1aravi <aravindvasamsetti@gmail.com>
2022-08-16 16:42:33 +0530

    chatpage, time format added

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const Registeruser = require('./model');
 const jwt = require('jsonwebtoken');
 const middleware = require('./middleware');
 const cors = require('cors');
+const Msgmodel = require('./Msgmodel');
 require("dotenv").config();
+
 
 const app = express();
 
@@ -90,3 +92,34 @@ app.listen(process.env.PORT || 5000, ()=>{
     console.log('Server running...')
 }
 )
+
+app.post('/addmsg', middleware, async(req, res) => {
+    try{
+        const {text} = req.body;
+        const exist = await Registeruser.findById(req.user.id)
+        let newmsg = new Msgmodel({
+            user: req.user.id,
+            username: exist.username,
+            text
+        })
+        await newmsg.save();
+        let allmsg = await Msgmodel.find();
+        return res.json(allmsg)
+   }
+   catch(err){
+       console.log(err);
+       return res.status(500).res.send('server error')
+   }
+
+})
+
+app.get('/getmsg', middleware, async(req, res)=>{
+    try{
+        let allmsg = await Msgmodel.find();
+        return res.json(allmsg)
+    }
+    catch(err){
+        console.log(err);
+        return res.status(500).res.send('server error')
+    }
+})
